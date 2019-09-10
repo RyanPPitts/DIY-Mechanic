e120b931a69d83828f7b394a4a9926fd77505070
RyanPPitts <47765262+RyanPPitts@users.noreply.github.com>
2019-09-10 14:19:30 -0700

    Updating landing page content

--- client/src/components/landingPage.js
+++ client/src/components/landingPage.js
@@ -7,24 +7,22 @@ class LandingPage extends Component {
     render(){
         return(
         
-            <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+        <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
 
       
-            <main role="main" class="inner cover">
-              <h1 class="cover-heading">Hi, this is the main page for DIY Mechanic</h1>
-              <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-            </main>
-    
-        
-        <div class="row">
-        <p class="col-sm"> This is more testing area</p>
-        </div>
-          <p> This way you dont need routes and can move freely within the app</p>
-          <p> Ideally this would be a mobile app</p>
-          <p> Best to minimize to the smallest view</p>
-        <h4 className='mb-3 ml-4'>Please log in to manage services</h4>
-        <button class='btn btn-outline-primary'><LoginModal/></button>
-        <button class='btn btn-outline-primary'><RegisterModal/></button>
+              <main role="main" class="inner cover">
+              <h1 class=" text-center text-uppercase">Welcome to DIY Mechanic</h1>
+              <p class="text-center"> This is v1.0 of the DIY Mechanic React App</p>
+              <p class="text-center"> Currently the app features token based login/registration, connection with MongoDB cloud server, simple CRUD operations on the services list</p>
+              <p class="text-center"> The app still needs the following : Styling, Ability to edit service, Ability to add "vehicles" to your garage, cleaner UI, ability to see upcoming services along with reminders</p>
+              <p class="text-center"> Currently utilizing all the React skills I have learned </p>
+              <h4 class="text-center">Please log in to manage services</h4>
+              </main>
+
+              <div class="d-flex  justify-content-center">
+              <button class='btn btn-outline-primary'><LoginModal/></button>
+              <button class='btn btn-outline-primary'><RegisterModal/></button>
+              </div>
         </div>
 
         );
